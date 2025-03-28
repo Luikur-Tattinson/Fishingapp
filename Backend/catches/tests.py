b9bd47132017f2d5d8686a95e36af1b3b478dfb5
LuikurTattinson <tonikeranen@protonmail.com>
2025-03-28 15:28:09 +0200

    Fixed the test

--- Backend/catches/tests.py
+++ Backend/catches/tests.py
@@ -1,15 +1,21 @@
 from django.test import TestCase
 from .models import CatchEntry
 from django.contrib.auth.models import User
+from datetime import date
 
 class CatchModelTest(TestCase):
     def test_string_representation(self):
         user = User.objects.create_user(username='fishfish', password='fishfish')
 
         catch = CatchEntry.objects.create(
-            species="StreetShark",
-            weight=2.5,
-            user=user
+            user = user,
+            area ='Oulu',
+            body_of_water = 'Oulujoki',
+            species = 'StreetShark',
+            weight = 2.5,
+            length = 900,
+            date_caught = date_today(),
+            image = 'test.jpg'
         )
 
         self.assertEqual(str(catch), "StreetShark (2.5 kg) by fishfish")
