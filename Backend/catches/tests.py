7ce676fc881b8f9839f1b055c928113a5f3efd75
LuikurTattinson <tonikeranen@protonmail.com>
2025-03-28 15:20:23 +0200

    Updated test

--- Backend/catches/tests.py
+++ Backend/catches/tests.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
 from .models import CatchEntry
+from django.contrib.auth.models import User
 
 class CatchModelTest(TestCase):
     def test_string_representation(self):
-        catch = CatchEntry(species="StreetShark", weight=2.5)
+        user = User.objects.create_user(username='fishfish', password='fishfish')
+
+        catch = CatchEntry.objects.create(
+            species="StreetShark",
+            weight=2.5,
+            user=user
+        )
+
         self.assertEqual(str(catch), "StreetShark (2.5 kg) by fishfish")
 
